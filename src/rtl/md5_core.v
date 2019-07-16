8b7b6a48ffa6ce9beb3b28aea80f85270abce86a
Joachim Strömbergson <joachim@assured.se>
2019-07-16 09:26:29 +0200

    Adding a lot of the expected functionality of the md5 core.

--- src/rtl/md5_core.v
+++ src/rtl/md5_core.v
@@ -40,14 +40,13 @@ module md5_core(
                 input wire            clk,
                 input wire            reset_n,
 
+                input wire            init,
                 input wire            next,
                 output wire           ready,
 
                 input wire [511 : 0]  block,
                 output wire [127 : 0] digest
                );
-
-
 
 
   //----------------------------------------------------------------
@@ -62,6 +61,8 @@ module md5_core(
   localparam CTRL_NEXT   = 2'h1;
   localparam CTRL_LOOP   = 2'h2;
   localparam CTRL_FINISH = 2'h3;
+
+  localparam NUM_ROUNDS = (64 - 1);
 
 
   //----------------------------------------------------------------
@@ -81,8 +82,8 @@ module md5_core(
   reg          ready_new;
   reg          ready_we;
 
-  reg [5 : 0]  round_ctr_reg;
-  reg [5 : 0]  round_ctr_new;
+  reg [6 : 0]  round_ctr_reg;
+  reg [6 : 0]  round_ctr_new;
   reg          round_ctr_inc;
   reg          round_ctr_rst;
   reg          round_ctr_we;
@@ -103,18 +104,75 @@ module md5_core(
   //----------------------------------------------------------------
   // Function declarations.
   //----------------------------------------------------------------
+  // F non linear function.
+  function [31 : 0] F(input [31 : 0] b, input [31 : 0] c,
+                      input [31 : 0] d, input [5 : 0] round);
+    begin
+      if (round < 16)
+        F = (b & c) | ((~b) & d);
+
+      if ((round >= 16) &&  (round < 32))
+        F = (d & b) | ((~d) & c);
+
+      if ((round >= 32) && (round < 48))
+        F = b ^ c ^ d;
+
+      if (round >= 48)
+        F = c ^ (b | (~d));
+    end
+  endfunction // F
+
+
+  // shamt
+  // Round based shift amount.
+  function [31 : 0] shamt(input [31 : 0] x, input [5 : 0] round);
+    begin
+      if (round < 16)
+        case(round[1 : 0])
+          0: shamt = {x[24 : 0], x[31 : 25]};
+          1: shamt = {x[19 : 0], x[31 : 20]};
+          2: shamt = {x[14 : 0], x[31 : 15]};
+          3: shamt = {x[09 : 0], x[31 : 10]};
+        endcase // case (x[1 : 0])
+
+      if ((round >= 16) && (round < 32))
+        case(round[1 : 0])
+          0: shamt = {x[26 : 0], x[31 : 27]};
+          1: shamt = {x[22 : 0], x[31 : 23]};
+          2: shamt = {x[17 : 0], x[31 : 18]};
+          3: shamt = {x[11 : 0], x[31 : 12]};
+        endcase // case (x[1 : 0])
+
+      if ((round >= 32) && (round < 48))
+        case(round[1 : 0])
+          0: shamt = {x[27 : 0], x[31 : 28]};
+          1: shamt = {x[20 : 0], x[31 : 21]};
+          2: shamt = {x[16 : 0], x[31 : 17]};
+          3: shamt = {x[08 : 0], x[31 : 09]};
+        endcase // case (x[1 : 0])
+
+      if (round >= 48)
+        case(round[1 : 0])
+          0: shamt = {x[25 : 0], x[31 : 26]};
+          1: shamt = {x[21 : 0], x[31 : 22]};
+          2: shamt = {x[16 : 0], x[31 : 17]};
+          3: shamt = {x[10 : 0], x[31 : 11]};
+        endcase // case (x[1 : 0])
+    end
+  endfunction // shamt
 
 
   //----------------------------------------------------------------
   // Instantiations.
   //----------------------------------------------------------------
-  md5_k_rom k_rom(.x(), .y(k));
+  md5_k_rom k_rom(.x(round_ctr_reg[5 : 0]), .y(k));
 
 
   //----------------------------------------------------------------
   // Concurrent connectivity for ports etc.
   //----------------------------------------------------------------
-  assign ready = ready_reg;
+  assign ready  = ready_reg;
+  assign digest = {a_reg, b_reg, c_reg, d_reg};
 
 
   //----------------------------------------------------------------
@@ -133,7 +191,7 @@ module md5_core(
           c_reg             <= 32'h0;
           d_reg             <= 32'h0;
           ready_reg         <= 1'h1;
-          round_ctr_reg     <= 6'h0;
+          round_ctr_reg     <= 7'h0;
           md5_core_ctrl_reg <= CTRL_IDLE;
         end
       else
@@ -146,6 +204,7 @@ module md5_core(
               a_reg <= a_new;
               b_reg <= b_new;
               c_reg <= c_new;
+              d_reg <= d_new;
             end
 
           if (round_ctr_we)
@@ -162,10 +221,9 @@ module md5_core(
   //----------------------------------------------------------------
   always @*
     begin : md5_datapath
-      reg [31 : 0] F;
-      reg [31 : 0] G;
-      reg [31 : 0] H;
-      reg [31 : 0] I;
+      reg [31 : 0] new_F;
+      reg [31 : 0] tmp_b;
+      reg [31 : 0] m;
 
       a_new  = 32'h0;
       b_new  = 32'h0;
@@ -173,23 +231,26 @@ module md5_core(
       d_new  = 32'h0;
       a_d_we = 1'h0;
 
-
-      F = (b_reg & c_reg) | (~b_reg & d_reg);
-      G = (b_reg & d_reg) | (c_reg & ~d_reg);
-      H = b_reg ^ c_reg ^ d_reg;
-      I = c_reg ^ (b_reg | ~d_reg);
-
+      m = block[31 : 0];
+      new_F = F(b_reg, c_reg, d_reg, round_ctr_reg[5 : 0]);
+      tmp_b = shamt((a_reg + new_F + m + k), round_ctr_reg[5 : 0]);
 
       if (dp_init)
         begin
-          a_reg = A0;
-          b_reg = B0;
-          c_reg = C0;
-          d_reg = D0;
+          a_new  = A0;
+          b_new  = B0;
+          c_new  = C0;
+          d_new  = D0;
+          a_d_we = 1'h1;
         end
 
       if (dp_update)
         begin
+          a_new  = d_reg;
+          b_new  = tmp_b;
+          c_new  = b_reg;
+          d_new  = c_reg;
+          a_d_we = 1'h1;
         end
     end
 
@@ -199,12 +260,12 @@ module md5_core(
   //----------------------------------------------------------------
   always @*
     begin : round_ctr
-      round_ctr_new = 6'h0;
+      round_ctr_new = 7'h0;
       round_ctr_we  = 1'h0;
 
       if (round_ctr_rst)
         begin
-          round_ctr_new = 6'h0;
+          round_ctr_new = 7'h0;
           round_ctr_we  = 1'h1;
         end
 
@@ -230,28 +291,42 @@ module md5_core(
       md5_core_ctrl_new = CTRL_IDLE;
       md5_core_ctrl_we  = 1'h0;
 
+
       case (md5_core_ctrl_reg)
         CTRL_IDLE:
           begin
             if (init)
               begin
                 dp_init       = 1'h0;
-                round_ctr_rst = 1'h1;
+              end
+
+            if (next)
+              begin
+                round_ctr_rst     = 1'h1;
+                ready_new         = 1'h0;
+                ready_we          = 1'h1;
+                md5_core_ctrl_new = CTRL_NEXT;
+                md5_core_ctrl_we  = 1'h1;
               end
           end
 
-        if (next)
-          begin
-            round_ctr_rst     = 1'h1;
-            md5_core_ctrl_new = CTRL_NEXT;
-            md5_core_ctrl_we  = 1'h1;
-          end
 
         CTRL_NEXT:
           begin
-            md5_core_ctrl_new = CTRL_IDLE;
-            md5_core_ctrl_we  = 1'h1;
+            if (round_ctr_reg == NUM_ROUNDS)
+              begin
+                ready_new         = 1'h1;
+                ready_we          = 1'h1;
+                md5_core_ctrl_new = CTRL_IDLE;
+                md5_core_ctrl_we  = 1'h1;
+              end
+            else
+              begin
+                dp_update     = 1'h1;
+                round_ctr_inc = 1'h1;
+              end
           end
+
 
         default:
           begin
